7d2c23c5b6d395f6259b4fa82b8e09ff0849edc1
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-07-10 19:55:23 -0400

    run script now is fully featured

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,3 +9,15 @@ git pull
 
 # run stack
 sudo docker-compose -f stack.yml -f ./traefik /traefik.yml up -d
+
+# go to home dir
+cd ~
+
+# go to plausible
+cd plausible
+
+# update repo with newest changes
+git pull
+
+# run stack
+docker-compose -f docker-compose.yml -f reverse-proxy/traefik/docker-compose.traefik.yml up -d
