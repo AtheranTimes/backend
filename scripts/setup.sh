4aa1eb6f4c63d8f4b682fc8e29acaa0f6b3e9bf2
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-07-10 18:51:53 -0400

    docker compose install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,8 +7,17 @@ cd ~
 # install docker
 curl -fsSL https://get.docker.com -o get-docker.sh | sh
 
+# download docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# apply perms
+sudo chmod +x /usr/local/bin/docker-compose
+
+# syslink
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 # download backend config
 git clone https://github.com/AtheranTimes/backend backend
 
-  # download plausible config
+# download plausible config
 git clone https://github.com/AtheranTimes/plausible-hosting plausible
